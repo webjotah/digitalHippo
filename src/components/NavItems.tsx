0fab6b864d44ce1df5b2363b36f40e9a620d6afa
webjotah <nascimento.joao@aluno.ifsp.edu.br>
2024-01-10 12:20:36 -0300

    finished shopping cart

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,39 @@
 'use client';
 
+import { PRODUCT_CATEGORIES } from '@/config';
 import { useState } from 'react';
+import NavItem from './NavItem';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-  return <div className="flex gap-4 h-full"></div>;
+  const isAnyOpen = activeIndex !== null;
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+
+        const isOpen = index === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+            key={category.value}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+{/*TOSTUDY: IntlNumberFormatOptions */}
+{/*TOSTUDY: Destructing elements in a Array */}
+
+export function formatPrice(
+  price: number | string,
+  options: { 
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "BRL",  
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+  const {currency = "BRL", notation = "compact"} = options
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
 }
